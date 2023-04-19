0f8bbf837bcda5cf4dcbc1429bfa24088dcbd6ec
Fomal9aut <sergeymarchenko903@gmail.com>
2023-04-19 12:09:13 +0300

    добавлены тесты для функции NumOfCombOfmFromN

--- NumOfCombOfmFromN_test/NumOfCombOfmFromN_test.cpp
+++ NumOfCombOfmFromN_test/NumOfCombOfmFromN_test.cpp
@@ -10,10 +10,38 @@ namespace NumOfCombOfmFromNtest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(FirstArgumentGreaterSecond)
 		{
-			uint value = NumOfCombOfmFromN(7,7);
-			uint exp_value = 7;
+			uint value = NumOfCombOfmFromN(9, 5);
+			uint exp_value = 126;
+			Assert::AreEqual(value, exp_value);
+		}
+
+		TEST_METHOD(ArgumentsAreEqual)
+		{
+			uint value = NumOfCombOfmFromN(5, 5);
+			uint exp_value = 1;
+			Assert::AreEqual(value, exp_value);
+		}
+
+		TEST_METHOD(SecondArgumentIsZero)
+		{
+			uint value = NumOfCombOfmFromN(9, 0);
+			uint exp_value = 1;
+			Assert::AreEqual(value, exp_value);
+		}
+
+		TEST_METHOD(ArgumentsAreZero)
+		{
+			uint value = NumOfCombOfmFromN(0, 0);
+			uint exp_value = 1;
+			Assert::AreEqual(value, exp_value);
+		}
+
+		TEST_METHOD(ComplexTest)
+		{
+			uint value = NumOfCombOfmFromN(9, 2);
+			uint exp_value = 36;
 			Assert::AreEqual(value, exp_value);
 		}
 	};
